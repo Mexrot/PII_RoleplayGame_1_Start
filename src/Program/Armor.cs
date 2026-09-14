714ce5a5cd075d5b59d3dd6f9b5a04940d2b495d
Erik <erikmendez432@gmail.com>
2026-09-13 22:46:03 -0300

    Armors 2.0

--- src/Program/Armor.cs
+++ src/Program/Armor.cs
@@ -1,9 +1,13 @@
-public class Helmet
+public class Armor
 {
+    public string Name { get; set; }
+    public int AttackValue { get; set; }
     public int DefenseValue { get; set; }
 
-    public Helmet(int defenseValue)
+    public Armor(string name, int defenseValue, int attackValue)
     {
+        Name = name;
         DefenseValue = defenseValue;
+        AttackValue = attackValue;
     }
 }
