e39b14ee2e012216abf2554021e50698764fe27b
Mexrot <137846579+Mexrot@users.noreply.github.com>
2026-09-13 23:21:04 -0300

    Algunos test

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -1,18 +1,90 @@
 using NUnit.Framework;
 
-namespace Ucu.Poo.RolePlayGame.Tests
+[TestFixture]
+public class WizardTests
 {
-    public class TestClass
+    [Test]
+    public void CalculateAttack_WizardWithSpellsBook_ReturnsTotalAttack()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        Spell spell1 = new Spell("Fireball", 30, 0);
+        Spell spell2 = new Spell("Lightning", 20, 0);
 
-        [Test]
-        public void TestMethod()
-        {
-            Assert.Pass();
-        }
+        SpellsBook book = new SpellsBook();
+        book.AddSpell(spell1);
+        book.AddSpell(spell2);
+
+        Wizard wizard = new Wizard("Gandalf", 100);
+        wizard.EquipSpellsBook(book);
+
+        Assert.That(wizard.CalculateAttack(), Is.EqualTo(50));
+    }
+
+    [Test]
+    public void CalculateDefense_WizardWithSpellsBook_ReturnsTotalDefense()
+    {
+        Spell spell1 = new Spell("Shield", 0, 20);
+        Spell spell2 = new Spell("Magic Barrier", 0, 15);
+
+        SpellsBook book = new SpellsBook();
+        book.AddSpell(spell1);
+        book.AddSpell(spell2);
+
+        Wizard wizard = new Wizard("Gandalf", 100);
+        wizard.EquipSpellsBook(book);
+
+        Assert.That(wizard.CalculateDefense(), Is.EqualTo(35));
+    }
+
+    [Test]
+    public void ReceiveAttack_AttackGreaterThanDefense_ReducesHealth()
+    {
+        Spell shield = new Spell("Shield", 0, 20);
+
+        SpellsBook book = new SpellsBook();
+        book.AddSpell(shield);
+
+        Wizard wizard = new Wizard("Gandalf", 100);
+        wizard.EquipSpellsBook(book);
+
+        wizard.ReceiveAttack(50);
+
+        Assert.That(wizard.Health, Is.EqualTo(70));
+    }
+
+    [Test]
+    public void ReceiveAttack_DefenseGreaterThanAttack_DoesNotReduceHealth()
+    {
+        Spell shield = new Spell("Shield", 0, 30);
+
+        SpellsBook book = new SpellsBook();
+        book.AddSpell(shield);
+
+        Wizard wizard = new Wizard("Gandalf", 100);
+        wizard.EquipSpellsBook(book);
+
+        wizard.ReceiveAttack(20);
+
+        Assert.That(wizard.Health, Is.EqualTo(100));
+    }
+
+    [Test]
+    public void ReceiveAttack_DamageGreaterThanHealth_HealthDoesNotGoBelowZero()
+    {
+        Wizard wizard = new Wizard("Gandalf", 100);
+
+        wizard.ReceiveAttack(150);
+
+        Assert.That(wizard.Health, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Cure_DamagedWizard_RestoresMaxHealth()
+    {
+        Wizard wizard = new Wizard("Gandalf", 100);
+
+        wizard.ReceiveAttack(40);
+        wizard.Cure();
+
+        Assert.That(wizard.Health, Is.EqualTo(100));
     }
 }
